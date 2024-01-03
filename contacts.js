cd1b53e0917340d8e2da7d59f3e5e4a5eb763c3a
raspiotr <raspiotr@gmail.com>
2024-01-03 23:40:37 +0100

    contacts.js functions ready

--- contacts.js
+++ contacts.js
@@ -1,11 +1,48 @@
 const fs = require("fs").promises;
 const path = require("path");
+const { nanoid } = require("nanoid");
 
 const contactsPath = path.join(__dirname, "db", "contacts.json");
 
 async function listContacts() {
-  const data = await fs.readFile(contactsPath);
-  console.log(data.toString());
+  const contacts = await fs.readFile(contactsPath);
+  const parsedContacts = JSON.parse(contacts);
+  console.log("Contacts list");
+  console.table(parsedContacts);
+  return parsedContacts;
 }
 
-module.exports = { listContacts };
+async function getContactById(contactId) {
+  const contacts = await fs.readFile(contactsPath);
+  const parsedContacts = JSON.parse(contacts);
+  const selectedContact =
+    parsedContacts.find((contact) => contact.id === contactId) || null;
+  console.log(`Selected contact (with ID: ${contactId})`);
+  console.table(selectedContact);
+  return selectedContact;
+}
+
+async function removeContact(contactId) {
+  const contacts = await fs.readFile(contactsPath);
+  const parsedContacts = JSON.parse(contacts);
+  const updatedContacts = parsedContacts.filter(
+    (contact) => contact.id !== contactId
+  );
+  console.log(`Updated contact list (removed contact with ID: ${contactId})`);
+  console.table(updatedContacts);
+  await fs.writeFile(contactsPath, JSON.stringify(updatedContacts, null, 1));
+  return updatedContacts;
+}
+
+async function addContact(name, email, phone) {
+  const contacts = await fs.readFile(contactsPath);
+  const parsedContacts = JSON.parse(contacts);
+  const newContact = { id: nanoid(), name, email, phone };
+  console.log(`Updated contact list (with new contact: ${name})`);
+  const updatedContacts = [...parsedContacts, newContact];
+  console.table(updatedContacts);
+  await fs.writeFile(contactsPath, JSON.stringify(updatedContacts, null, 1));
+  return newContact;
+}
+
+module.exports = { listContacts, getContactById, removeContact, addContact };
--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 const contacts = require("./contacts");
 
 contacts.listContacts();
+contacts.getContactById("05olLMgyVQdWRwgKfg5J6");
+contacts.removeContact("qdggE76Jtbfd9eWJHrssH");
+contacts.addContact("Mango", "mango@gmail.com", "322-22-22");
