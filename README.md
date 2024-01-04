6c70ffb752345b59957fcd427babec1d8d6b849f
raspiotr <raspiotr@gmail.com>
2024-01-04 12:02:04 +0100

    ready

---
+++ README.md
@@ -0,0 +1,19 @@
+# Otrzymujemy i wyprowadzamy całą listę kontaktów w postaci tabeli (console.table)
+
+node index.js --action list
+Wynik: https://ibb.co/pZmF3zS
+
+# Otrzymujemy kontakt po id
+
+node index.js --action get --id 05olLMgyVQdWRwgKfg5J6
+Wynik: https://ibb.co/M6YP6JB
+
+# Dodajemy kontakt
+
+node index.js --action add --name Mango --email mango@gmail.com --phone 322-22-22
+Wynik: https://ibb.co/w4jQTKw
+
+# Usuwamy kontakt
+
+node index.js --action remove --id qdggE76Jtbfd9eWJHrssH
+Wynik: https://ibb.co/gmPjNpT
--- contacts.js
+++ contacts.js
@@ -1,15 +1,15 @@
 const fs = require("fs").promises;
 const path = require("path");
 const { nanoid } = require("nanoid");
+require("colors");
 
 const contactsPath = path.join(__dirname, "db", "contacts.json");
 
 async function listContacts() {
   const contacts = await fs.readFile(contactsPath);
   const parsedContacts = JSON.parse(contacts);
-  console.log("Contacts list");
+  console.log("Contacts list:".yellow);
   console.table(parsedContacts);
-  return parsedContacts;
 }
 
 async function getContactById(contactId) {
@@ -17,9 +17,8 @@ async function getContactById(contactId) {
   const parsedContacts = JSON.parse(contacts);
   const selectedContact =
     parsedContacts.find((contact) => contact.id === contactId) || null;
-  console.log(`Selected contact (with ID: ${contactId})`);
+  console.log(`Selected contact (with ID: ${contactId}):`.blue);
   console.table(selectedContact);
-  return selectedContact;
 }
 
 async function removeContact(contactId) {
@@ -28,21 +27,21 @@ async function removeContact(contactId) {
   const updatedContacts = parsedContacts.filter(
     (contact) => contact.id !== contactId
   );
-  console.log(`Updated contact list (removed contact with ID: ${contactId})`);
+  console.log(
+    `Updated contact list (removed contact with ID: ${contactId}):`.red
+  );
   console.table(updatedContacts);
   await fs.writeFile(contactsPath, JSON.stringify(updatedContacts, null, 1));
-  return updatedContacts;
 }
 
 async function addContact(name, email, phone) {
   const contacts = await fs.readFile(contactsPath);
   const parsedContacts = JSON.parse(contacts);
   const newContact = { id: nanoid(), name, email, phone };
-  console.log(`Updated contact list (with new contact: ${name})`);
+  console.log(`Updated contact list (with new contact: ${name}):`.green);
   const updatedContacts = [...parsedContacts, newContact];
   console.table(updatedContacts);
   await fs.writeFile(contactsPath, JSON.stringify(updatedContacts, null, 1));
-  return newContact;
 }
 
 module.exports = { listContacts, getContactById, removeContact, addContact };
